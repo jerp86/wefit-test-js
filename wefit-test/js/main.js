5204f40d911ef03860a89b61cd8b9c8fda581751
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-02 23:32:36 -0300

    feat: update header

--- wefit-test/js/main.js
+++ wefit-test/js/main.js
@@ -4,4 +4,25 @@ const changeMenuToHorizontal = () => {
   divMenu.setAttribute('aria-label', 'Horizontal button group')
 }
 
+const changeButtonHeader = () => {
+  const buttonHeader = document.querySelector('a.btn.btn-primary.btn-lg')
+  buttonHeader.classList.remove('btn-primary')
+  buttonHeader.classList.add('btn-success')
+}
+
+const changeHeaderStyles = () => {
+  const header = document.querySelector('.jumbotron')
+  header.classList.add('text-right')
+  header.style.cssText = `
+    background-color: var(--gray);
+    color: var(--white);
+  `
+}
+
+const changeHeaderBox = () => {
+  changeButtonHeader()
+  changeHeaderStyles()
+}
+
 changeMenuToHorizontal()
+changeHeaderBox()
